a16645b524c63ff3ebeb54f34754a9d508ba8f82
Daniel Groos <daniegr@stud.ntnu.no>
2016-03-31 13:09:25 +0200

    Starting to add attributes and basic methods to models

--- core/src/no/pag6/models/Player.java
+++ core/src/no/pag6/models/Player.java
@@ -1,4 +1,18 @@
 package no.pag6.models;
 
 public class Player {
+    private int score = 0;
+    private PlayerCharacter gameCharacter;
+
+    public Player(PlayerCharacter character){
+        gameCharacter = character;
+    }
+
+    public void setScore(int newScore){
+        score = newScore;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
