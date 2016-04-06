1df793c0850c6f200f41cd4a47a81bc7a5127017
Dag Erik Løvgren <dagerikhl@gmail.com>
2016-04-07 01:00:25 +0200

    Remove dummy-assets and dummy-code

--- core/src/no/pag6/helpers/AssetLoader.java
+++ core/src/no/pag6/helpers/AssetLoader.java
@@ -2,25 +2,23 @@ package no.pag6.helpers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AssetLoader {
 
     public static Preferences prefs;
 
-    public static Texture logo_texture;
-    public static TextureRegion logo, bird, bird_down, bird_up;
+    // Textures and TextureRegions
+//    public static Texture ;
+//    public static TextureRegion ;
 
-    public static Animation bird_animation;
+    // Animations
+//    public static Animation ;
 
-    public static Sound dead;
+    // Sounds
+//    public static Sound ;
 
-    public static BitmapFont font, shadow;
+    // Fonts
+//    public static BitmapFont ;
 
     public static void load() {
         // Preferences
@@ -30,39 +28,27 @@ public class AssetLoader {
         }
 
         // Textures and TextureRegions
-        logo_texture = new Texture(Gdx.files.internal("textures/badlogic.png"));
-        logo_texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+//         = new Texture(Gdx.files.internal("textures/.png"));
+//        .setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
-        logo = new TextureRegion(logo_texture, 0, 0);
+//         = new TextureRegion(, 0, 0);
+//        .flip(false, true);
 
-        bird_down = new TextureRegion(logo_texture, 0, 0, 10, 10);
-        bird_down.flip(false, true);
-
-        bird = new TextureRegion(logo_texture, 0, 0, 50, 50);
-        bird.flip(false, true);
-
-        bird_up = new TextureRegion(logo_texture, 0, 0, 100, 100);
-        bird_up.flip(false, true);
-
-        TextureRegion[] birds = {bird_down, bird, bird_up};
-        bird_animation = new Animation(0.06f, birds);
-        bird_animation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+        // Animations
+//        TextureRegion[]  = {};
+//         = new Animation(0.06f, );
+//        .setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 
         // Sounds
-        dead = Gdx.audio.newSound(Gdx.files.internal("sounds/dead.wav"));
+//         = Gdx.audio.newSound(Gdx.files.internal("sounds/.wav"));
 
         // Fonts
-        font = new BitmapFont(Gdx.files.internal("fonts/text.fnt"));
-        font.getData().setScale(.25f, -.25f);
-        shadow = new BitmapFont(Gdx.files.internal("fonts/shadow.fnt"));
-        shadow.getData().setScale(.25f, -.25f);
+//         = new BitmapFont(Gdx.files.internal("fonts/.fnt"));
+//        .getData().setScale(.25f, -.25f);
     }
 
     public static void dispose() {
-        logo_texture.dispose();
-        dead.dispose();
-        font.dispose();
-        shadow.dispose();
+//        .dispose();
     }
 
     public static int getHighScore() {
