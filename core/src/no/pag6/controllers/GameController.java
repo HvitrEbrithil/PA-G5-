51c5f320c7e4b964e188d42c9addc4540ea5ee39
Dag Erik Løvgren <dagerikhl@gmail.com>
2016-04-07 00:53:27 +0200

    Set up controllers and views

--- core/src/no/pag6/controllers/GameController.java
+++ core/src/no/pag6/controllers/GameController.java
@@ -1,4 +1,92 @@
 package no.pag6.controllers;
 
 public class GameController {
+
+    public enum GameState {
+        RUNNING, PAUSED, GAME_OVER
+    }
+
+    private GameState currentState;
+
+    private float runTime = 0;
+
+    private int nofPlayers;
+    private int p1Score = 0;
+    private int p2Score = 0;
+
+    public GameController(int nofPlayers) {
+        this.nofPlayers = nofPlayers;
+
+        currentState = GameState.RUNNING;
+    }
+
+    public void update(float delta) {
+        runTime += delta;
+
+        switch (currentState) {
+            case RUNNING:
+                updateRunning(delta);
+                break;
+            case PAUSED:
+                updatePaused(delta);
+                break;
+            case GAME_OVER:
+                updateGameOver(delta);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void run() {
+        currentState = GameState.RUNNING;
+    }
+
+    public void pause() {
+        currentState = GameState.PAUSED;
+    }
+
+    public void end() {
+        currentState = GameState.GAME_OVER;
+    }
+
+    public int getNofPlayers() {
+        return nofPlayers;
+    }
+
+    public int getP1Score() {
+        return p1Score;
+    }
+    public void addP1Score(int increment) {
+        p1Score += increment;
+    }
+
+    public int getP2Score() {
+        return p2Score;
+    }
+    public void addP2Score(int increment) {
+        p2Score += increment;
+    }
+
+    public boolean isRunning() {
+        return currentState == GameState.RUNNING;
+    }
+
+    public boolean isPaused() {
+        return currentState == GameState.PAUSED;
+    }
+
+    public boolean isGameOver() {
+        return currentState == GameState.GAME_OVER;
+    }
+
+    private void updateRunning(float delta) {
+    }
+
+    private void updatePaused(float delta) {
+    }
+
+    private void updateGameOver(float delta) {
+    }
+
 }
--- core/src/no/pag6/controllers/MenuController.java
+++ core/src/no/pag6/controllers/MenuController.java
@@ -1,4 +1,67 @@
 package no.pag6.controllers;
 
 public class MenuController {
+
+    public enum MenuState {
+        MAIN_MENU, OPTIONS_MENU, HIGHSCORE_MENU
+    }
+
+    private MenuState currentState;
+
+    private float runTime = 0;
+
+    public MenuController() {
+        currentState = MenuState.MAIN_MENU;
+    }
+
+    public void update(float delta) {
+        runTime += delta;
+
+        switch (currentState) {
+            case MAIN_MENU:
+                updateMainMenu(delta);
+                break;
+            case OPTIONS_MENU:
+                updateOptionsMenu(delta);
+                break;
+            case HIGHSCORE_MENU:
+                updateHighscoreMenu(delta);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void goToMainMenu() {
+        currentState = MenuState.MAIN_MENU;
+    }
+
+    public void goToOptionsMenu() {
+        currentState = MenuState.OPTIONS_MENU;
+    }
+
+    public void goToHighscoreMenu() {
+        currentState = MenuState.HIGHSCORE_MENU;
+    }
+
+    public boolean isMainMenu() {
+        return currentState == MenuState.MAIN_MENU;
+    }
+
+    public boolean isOptionsMenu() {
+        return currentState == MenuState.OPTIONS_MENU;
+    }
+
+    public boolean isHighscoreMenu() {
+        return currentState == MenuState.HIGHSCORE_MENU;
+    }
+
+    private void updateMainMenu(float delta) {
+    }
+
+    private void updateOptionsMenu(float delta) {
+    }
+
+    private void updateHighscoreMenu(float delta) {
+    }
 }
