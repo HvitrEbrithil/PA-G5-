10bb15d2fc51a57791a8e2b2df94a304c5259ac7
Dag Erik <dagerikhl@gmail.com>
2016-04-07 13:33:09 +0200

    Added dummyfolder for Assets

--- core/src/no/pag6/models/states/State.java
+++ core/src/no/pag6/models/states/State.java
@@ -6,36 +6,29 @@ public class State implements Screen {
 
     @Override
     public void show() {
-
     }
 
     @Override
     public void render(float delta) {
-
     }
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
     public void pause() {
-
     }
 
     @Override
     public void resume() {
-
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
     public void dispose() {
-
     }
 }
