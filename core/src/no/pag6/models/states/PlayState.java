2855804a1047d96ce55d052265830112bbc51a25
TobiasAsk <arnebjarne1337@hotmail.com>
2016-04-12 18:45:38 +0200

    Added constructor that loads Tiled maps

--- core/src/no/pag6/models/states/PlayState.java
+++ core/src/no/pag6/models/states/PlayState.java
@@ -1,6 +1,25 @@
 package no.pag6.models.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 import no.pag6.controllers.GameController;
 import no.pag6.game.PAG6Game;
 import no.pag6.views.GameRenderer;
@@ -10,17 +29,123 @@ public class PlayState extends State {
     private GameController gameController;
     private GameRenderer gameRenderer;
 
+    // camera stuff
+    OrthographicCamera gameCamera;
+    Viewport viewPort;
+    int V_WIDTH = 800, V_HEIGHT = 480;
+    float PPM = 100;
+
+    // map stuff
+    TiledMap map;
+    OrthogonalTiledMapRenderer mapRenderer;
+
+    // box2d stuff
+    World world;
+    Box2DDebugRenderer b2dr;
+    Body playerBody;
+    Vector2 gravity = new Vector2(0, -10);
+
+    // constants
+    final static String MAP_FILE_NAME = "duude", FIRST_LAYER_NAME = "jau",
+            SECOND_LAYER_NAME = "hau";
+    final static String[] LAYERS = {FIRST_LAYER_NAME, SECOND_LAYER_NAME};
+
+    final static short FIRST_LAYER_BITS = 2, SECOND_LAYER_BITS = 4;
+    final static short[] FILTER_BITS = {FIRST_LAYER_BITS, SECOND_LAYER_BITS};
+
+    final static int INIT_PLAYER_POS_X = 20, INIT_PLAYER_POS_Y = 200,
+                    PLAYER_BODY_RADIUS = 10;
+
+    final static float TIME_STEP = 1/60f;
+
     public PlayState(PAG6Game game, int nofPlayers) {
         gameController = game.getGameController();
         gameController.setNofPlayers(nofPlayers);
         gameRenderer = game.getGameRenderer();
         Gdx.input.setInputProcessor(game.getGameInputHandler());
+
+        // set up the camera
+        gameCamera = new OrthographicCamera();
+        viewPort = new FitViewport(V_WIDTH / PPM, V_HEIGHT / PPM, gameCamera);
+
+        // load the map
+        map = new TmxMapLoader().load(MAP_FILE_NAME);
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / PPM);
+
+        // set up box2d
+        world = new World(gravity, true);
+        b2dr = new Box2DDebugRenderer();
+        addMapBodies();
+        addPlayerBody();
     }
 
     @Override
     public void render(float delta) {
+        update(delta);
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        mapRenderer.render();
+
         gameController.update(delta);
         gameRenderer.render(delta);
+    }
+
+    private void addMapBodies() {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fixtureDef = new FixtureDef();
+        Body body;
+
+        // loop through the layers in the map and add box2d bodies to the box2d world
+        for (int i = 0; i < LAYERS.length; i++) {
+            for (MapObject mapObject : map.getLayers().get(LAYERS[i]).getObjects()
+                                                .getByType(RectangleMapObject.class)) {
+
+                Rectangle rect = ((RectangleMapObject) mapObject).getRectangle();
+                bodyDef.position.set(
+                        (rect.getX() + rect.getWidth() / 2) / PPM,
+                        (rect.getY() + rect.getHeight() / 2) / PPM);
+                body = world.createBody(bodyDef);
+                shape.setAsBox(rect.getWidth() / (2*PPM), rect.getHeight() / (2*PPM));
+                fixtureDef.shape = shape;
+                fixtureDef.filter.categoryBits = FILTER_BITS[i];
+                body.createFixture(fixtureDef);
+            }
+        }
+        shape.dispose();
+    }
+
+    private void addPlayerBody() {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(INIT_PLAYER_POS_X / PPM, INIT_PLAYER_POS_Y / PPM);
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        playerBody = world.createBody(bodyDef);
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(PLAYER_BODY_RADIUS / PPM);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.filter.maskBits = FIRST_LAYER_BITS; // the player starts in lane 1
+        playerBody.createFixture(fixtureDef);
+    }
+
+    private void update(float delta) {
+        handleInput(delta);
+        world.step(TIME_STEP, 6, 2);
+
+        gameCamera.position.x = playerBody.getPosition().x;
+        gameCamera.update();
+
+        mapRenderer.setView(gameCamera);
+    }
+
+    private void handleInput(float delta) {
+
     }
 
 }
