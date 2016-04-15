92217ae66d57a560273c6693478090dc38b8319c
TobiasAsk <arnebjarne1337@hotmail.com>
2016-04-15 11:46:37 +0200

    Fixed bug in splashscreen render method

--- core/src/no/pag6/states/SplashScreen.java
+++ core/src/no/pag6/states/SplashScreen.java
@@ -41,6 +41,7 @@ public class SplashScreen extends State {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        game.spriteBatch.setProjectionMatrix(cam.combined);
         game.spriteBatch.begin();
         logo.draw(game.spriteBatch);
         game.spriteBatch.end();
