330a7f0361add520fefeda647bc94bcc5402134d
Dag Erik <dagerikhl@gmail.com>
2016-04-15 13:04:50 +0200

    Created Constants interface

--- core/src/no/pag6/game/PAG6Game.java
+++ core/src/no/pag6/game/PAG6Game.java
@@ -3,9 +3,7 @@ package no.pag6.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 import no.pag6.helpers.AssetLoader;
-import no.pag6.states.PlayState;
 import no.pag6.states.SplashScreen;
 import no.pag6.states.State;
 
@@ -14,9 +12,6 @@ import java.util.Stack;
 public class PAG6Game extends Game {
 
     public static final String TAG = "PAG6Game";
-	public static final int V_WIDTH = 800, V_HEIGHT = 480;
-	public static final float PPM = 100;
-	public static float TIME_STEP = 1/60f;
 
 	public SpriteBatch spriteBatch;
 	public Stack<State> gameStack;
@@ -32,7 +27,7 @@ public class PAG6Game extends Game {
 		gameStack = new Stack<State>();
 		SplashScreen splashScreen = new SplashScreen(this);
 		gameStack.push(splashScreen);
-        setScreen(new PlayState(this, 2, "test_lvl.tmx"));
+        setScreen(splashScreen);
     }
 
 	@Override
